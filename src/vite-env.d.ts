e923459423c03819b440307f5336f58955d871bf
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-09-16 21:08:10 +0530

    feat: setup environments for the project

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_ENV: 'production' | 'development' | 'test'
+    readonly VITE_BACKEND_PROXY: string
+    // more env variables...
+}
+
+export interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
