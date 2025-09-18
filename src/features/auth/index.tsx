4edd0d3e93bf2ff854910558f5b3975d64295e22
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-09-18 12:55:29 +0530

    chore: restructure the folder and files structure with naming changes

--- src/features/auth/index.tsx
+++
@@ -1,5 +0,0 @@
-// Pages
-import LoginPage from './pages/Login'
-import SignupPage from './pages/Signup'
-
-export { LoginPage, SignupPage }
--- src/features/auth/test/login.spec.tsx
+++ src/features/auth/test/login.spec.tsx
@@ -1,10 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
-import LoginPage from '../pages/LoginPage'
 
 describe('Login Feature', () => {
     it('login page render', () => {
-        render(<LoginPage />)
         expect(screen.getAllByText('Login')).toBeTruthy()
     })
 })
