9d7d8a24ab64108f97c62b9898a54f4935618fc5
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-09-19 17:05:35 +0530

    feat: add import/order rules for consistency and readability

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
 
 export default tseslint.config([
     globalIgnores(['dist', 'node_modules', 'build', 'coverage']),
@@ -21,8 +22,32 @@ export default tseslint.config([
             ecmaVersion: 2020,
             globals: globals.browser
         },
+        plugins: {
+            import: importPlugin
+        },
         rules: {
-            'no-console': 'error'
+            'no-console': 'error',
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            'import/order': [
+                'error',
+                {
+                    groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+                    pathGroups: [
+                        {
+                            pattern: 'react',
+                            group: 'external',
+                            position: 'before'
+                        },
+                        {
+                            pattern: '@/**',
+                            group: 'internal',
+                            position: 'after'
+                        }
+                    ],
+                    alphabetize: { order: 'asc', caseInsensitive: true },
+                    'newlines-between': 'always'
+                }
+            ]
         }
     }
 ])
