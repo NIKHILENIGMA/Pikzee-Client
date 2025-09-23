1c282bcf0f54feb6de2f2f7af169b41c2b90b5ed
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-09-23 17:08:53 +0530

    feat: export ChooseOptions, EmailLogin, EmailSignup, and SignupVerificationCode in index

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,8 @@
+import ChooseOptions from './components/choose-options'
+import EmailLogin from './components/email-login'
+import EmailSignup from './components/email-signup'
 import LoginForm from './components/login-form'
+import SignupForm from './components/signup-form'
+import SignupVerificationCode from './components/signup-verification-code'
 
-export { LoginForm }
+export { ChooseOptions, EmailLogin, EmailSignup, LoginForm, SignupForm, SignupVerificationCode }
