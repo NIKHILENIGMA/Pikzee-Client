74a63aab2d77fda014eb90ff02dc0415b1ef05f0
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-09-23 16:22:30 +0530

    feat: import signupSchema and define SignupFormRequest type

--- src/features/auth/types/auth.ts
+++ src/features/auth/types/auth.ts
@@ -1,6 +1,8 @@
 import type z from 'zod'
 
-import { loginSchema } from '@/shared/schema/auth-schema'
+import { loginSchema, signupSchema } from '@/shared/schema/auth-schema'
 
 // Infer the LoginFormRequest type from the loginSchema
 export type LoginFormRequest = z.infer<typeof loginSchema>
+
+export type SignupFormRequest = z.infer<typeof signupSchema>
