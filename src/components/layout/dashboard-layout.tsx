660478d4a67e6eed0c6e8822e6c3ca14ee5ecb16
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-09-28 19:38:28 +0530

    feat: add ContainLoader component and update exports in index.tsx

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -5,9 +5,9 @@ import Sidebar from '../shared/sidebar'
 
 const DashboardLayout: FC = () => {
     return (
-        <div className="w-full h-screen flex">
+        <div className="w-full flex">
             <Sidebar />
-            <div className="flex-1 p-4">
+            <div className="flex-1 w-[95%] min-h-screen overflow-y-auto">
                 {/* Main content goes here */}
                 <Outlet />
             </div>
